cb71a5881a987b0dd18d7de8f8002b476b30fb2f
Ian P <41635087+ianz56@users.noreply.github.com>
2022-12-26 15:39:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    API_KEY_RM_BG: "YOUR_API_KEY",
-    API_KEY_OPEN_AI: "YOUR_API_KEY"
+    API_KEY_RM_BG: "L4MFi3eJjLMu2xApuUiPD1UY",
+    API_KEY_OPEN_AI: "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"
 }
